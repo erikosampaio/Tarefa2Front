82b4374f361279011e53002faf1c2404e584dcf2
Eriko Sampaio <erikoa.93@gmail.com>
2022-05-14 19:13:11 -0300

    adicionado botão

--- java.js
+++ java.js
@@ -1,3 +1,4 @@
+// Função para criar tabela
 function formulario() {
     let vinculo = document.getElementById("vinculo").value;
     let autor = document.getElementById("autor").value;
@@ -6,7 +7,9 @@ function formulario() {
     let titulo = document.getElementById("titulo").value;
     let edicao = document.getElementById("edicao").value;
     let ano = document.getElementById("ano").value;
+
     var frutas = [vinculo, titulo, autor, edicao, issn, ano, editora];
+
     document.getElementById("tab_vinculo").innerHTML = frutas[0];
     document.getElementById("tab_titulo").innerHTML = frutas[1];
     document.getElementById("tab_autor").innerHTML = frutas[2];
@@ -16,10 +19,7 @@ function formulario() {
     document.getElementById("tab_editora").innerHTML = frutas[6];
 }
 
-function remove() {
-    frutas.clear();
-}
-
+//Funções para cronômetro
 function startTimer(duration, display) {
 
     var timer = duration, minutes, seconds;
@@ -35,18 +35,36 @@ function startTimer(duration, display) {
         display.textContent = minutes + ":" + seconds;
 
         if (--timer < 0) {
-            timer = duration;
-            if (timer == 0) {
-                document.getElementById("button").disabled = true;
-            }
+            timer = 0
+            document.querySelector("#vinculo").disabled = true
+            document.querySelector("#autor").disabled = true
+            document.querySelector("#issn").disabled = true
+            document.querySelector("#editora").disabled = true
+            document.querySelector("#titulo").disabled = true
+            document.querySelector("#edicao").disabled = true
+            document.querySelector("#ano").disabled = true
         }
+
     }, 1000);
 }
 
 window.onload = function () {
 
-    var duration = 3; // Conversão para segundos
+    var duration = (60 * 60) - 1; // Conversão para segundos
     var display = document.querySelector("#timer"); // Elemento para exibir o timer
 
     startTimer(duration, display); //Inicia a função
+}
+
+
+
+//Função para validar campos obrigatórios
+
+// Vinculo, Titulo, Autor
+
+
+
+// Ano
+if (document.querySelector("#ano" < 1900 && "#ano" > 2019)) {
+    let
 }

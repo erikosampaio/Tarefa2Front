e95edfe8b6635ec86b9e780cfc10c81a5daab98e
Eriko <erikoa.93@gmail.com>
2022-05-16 10:49:18 -0300

    mudado implementação de tabela. Falta pensa em como fazer

--- java.js
+++ java.js
@@ -1,60 +1,17 @@
-class Formulario {
+function salvar() {
+    let vinculo = document.querySelector("#vinculo").value;
+    let autor = document.querySelector("#autor").value;
+    let issn = document.querySelector("#issn").value;
+    let editora = document.querySelector("#editora").value;
+    let titulo = document.querySelector("#titulo").value;
+    let edicao = document.querySelector("#edicao").value;
+    let ano = document.querySelector("#ano").value;
 
-    constructor() {
-        this.arraySolicitacao = [];
-    }
+    console.log('Deu certo');
 
-    salvar() {
-        let formulario = this.lerDados();
-
-        if (this.validaCampos(formulario)) {
-            this.adicionar(formulario);
-        }
-
-        console.log(this.arraySolicitacao);
-    }
-
-    adicionar(formulario) {
-        this.arraySolicitacao.push(formulario);
-    }
-
-    lerDados() {
-        let formulario = {}
-
-        formulario.vinculo = document.getElementById('vinculo').value;
-        formulario.autor = document.getElementById("autor").value;
-        formulario.issn = document.getElementById("issn").value;
-        formulario.editora = document.getElementById("editora").value;
-        formulario.titulo = document.getElementById("titulo").value;
-        formulario.edicao = document.getElementById("edicao").value;
-        formulario.ano = document.getElementById("ano").value;
-
-        return formulario;
-    }
-
-    validaCampos() {
-        let msg = '';
-
-        if (formulario.vinculo == '') {
-            msg += '- Informe o vínculo \n';
-        }
-
-        if (formulario.autor == '') {
-            msg += '- Informe o vínculo \n';
-        }
-
-        if (formulario.titulo == '') {
-            msg += '- Informe o vínculo \n';
-        }
-
-        if (msg != '') {
-            alert(msg);
-            return false;
-        }
-
-        return true;
-    }
+    
 }
+
 
 //Funções para cronômetro
 function startTimer(duration, display) {
